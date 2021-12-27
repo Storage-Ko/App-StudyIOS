7bcc8d99ac94191231f9df2b14f7ea78dd7e5a6b
Ko-GyeongTae <kokt0203@naver.com>
2021-12-27 23:38:15 +0900

    FINISH Weather App

---
+++ WeatherApp/WeatherApp/ErrorMessage.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorMessage.swift
+//  WeatherApp
+//
+//  Created by gyeongtae on 2021/12/27.
+//
+
+import Foundation
+
+struct ErrorMessage: Codable {
+    let message: String
+}
---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by gyeongtae on 2021/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var weatherStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))°C"
+        self.minTempLabel.text = "최저: \(Int(weatherInformation.temp.minTemp - 273.15))°C"
+        self.maxTempLabel.text = "최고: \(Int(weatherInformation.temp.maxTemp - 273.15))°C"
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "오류", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func getCurrentWeather(cityName: String){
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=d8540a8ab16db84ee29fb45735a6b686") else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url){ [weak self] data, response, error in
+            let successRange = (200..<300)
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+            } else {
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    self?.showAlert(message: errorMessage.message)
+                }
+            }
+        }.resume()
+    }
+    
+}
+
