b039077b2f2c4f6e83d18cc8ad4d3040481cd5d3
Ko-GyeongTae <kokt0203@naver.com>
2021-12-27 09:27:20 +0900

    ADD - lifeCycle

--- ScreenTransaction/ScreenTransaction/SeguePushViewController.swift
+++ ScreenTransaction/ScreenTransaction/SeguePushViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 class SeguePushViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+      super.viewDidLoad()
+      print("SeguePushViewController 뷰가 로드 되었다.")
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      print("SeguePushViewController 뷰가 나타날 것이다.")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+      print("SeguePushViewController 뷰가 나타났다.")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      print("SeguePushViewController 뷰가 사라질 것이다.")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+      super.viewDidDisappear(animated)
+      print("SeguePushViewController 뷰가 사라졌다.")
+    }
+
     @IBAction func tapBackButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
--- ScreenTransaction/ScreenTransaction/ViewController.swift
+++ ScreenTransaction/ScreenTransaction/ViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      print("ViewController 뷰가 로드 되었다.")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      print("ViewController 뷰가 나타날 것이다.")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+      print("ViewController 뷰가 나타났다.")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      print("ViewController 뷰가 사라질 것이다.")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+      super.viewDidDisappear(animated)
+      print("ViewController 뷰가 사라졌다.")
     }
     @IBAction func tapCodePushButton(_ sender: UIButton) {
         guard let viewController = self.storyboard?.instantiateViewController(identifier: "CodePushViewController")
